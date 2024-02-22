89578820c4e00486fc8882c4f94ac99021d94916
b-regan11 <reganbill02@gmail.com>
2024-02-22 16:13:59 -0500

    Sorted Product Page

--- src/views/ProductPages/Outdoors/GatoradeBottle.vue
+++ src/views/ProductPages/Outdoors/GatoradeBottle.vue
@@ -3,7 +3,7 @@
         <br>
         <h1>Gatorade Bottle</h1>
         <a href="https://www.amazon.com/Gatorade-Gx-Marble-Blue-30/dp/B0BXFM9HRJ/ref=sr_1_1_sspa?crid=31Q05ABGMWZBU&keywords=water%2Bbottle&qid=1707707389&sprefix=water%2Bbottle%2Caps%2C117&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1">
-            <img src="../../../assets/ProductImages/GatoradeBottle.jpg" alt="Gatorade Bottle" style="width: 100px">
+            <img src="../../../assets/ProductImages/GatoradeBottle.jpg" alt="Gatorade Bottle" style="width: 150px">
         </a>
         <p>Fuel your game with Gatorades new customizable hydration platform.</p>
         <a href="https://www.amazon.com/Gatorade-Gx-Marble-Blue-30/dp/B0BXFM9HRJ/ref=sr_1_1_sspa?crid=31Q05ABGMWZBU&keywords=water%2Bbottle&qid=1707707389&sprefix=water%2Bbottle%2Caps%2C117&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1">Click For More</a>
