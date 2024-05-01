53ef84366996bcabf8f5d3f015b56472504f6166
b-regan11 <reganbill02@gmail.com>
2024-05-01 19:46:30 -0400

    Config Adjustment

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     publicPath: process.env.NODE_ENV === 'production'
       ? '/Ecommerce-Site/'
-      : '/Ecommerce-Site/',
+      : '/',
     transpileDependencies: true
 })
